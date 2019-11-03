d30f3c81b5be7eee86d8308421bfbec5163fc7ba
Sandeep Kota <sandeep.kota1997@gmail.com>
2019-11-03 01:59:20 -0400

    [talker] Added ROS_STREAM messages

--- src/talker.cpp
+++ src/talker.cpp
@@ -25,7 +25,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * @file    talker.cpp
  * @author  Sandeep Kota
- * @version 1.0
+ * @version 2.0
  * @brief Publisher node
  * @section DESCRIPTION
  * C++ program to publish the topic "chatter" containing custom string message
@@ -35,9 +35,10 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "beginner_tutorials/updateString.h"
+#include <string>
 
-/// An external string message to make it accesible in different scope 
-extern std::string msg = "Initial Message";
+/// An external string message to make it accesible in different scope
+extern std::string text = "Initial Message";
 
 /**
  * @brief A function to update the default string of talker
@@ -47,51 +48,74 @@ extern std::string msg = "Initial Message";
  */
 bool update(beginner_tutorials::updateString::Request &request,
             beginner_tutorials::updateString::Response &response) {
-  /// Update the string
-  response.updatedString = request.newString;
-  msg = response.updatedString;
-  return true;
+    /// Update the string
+    response.updatedString = request.newString;
+    text = response.updatedString;
+    return true;
 }
 
 /**
- * @brief Main function implementation to publish to 'chatter' topic and 
- * print the message
+ * @brief Main function implementation to publish to 'chatter' topic and
+ * print the message and run a service simultaneously to change the sring message
  * @param argc Number of parameters passed in command line
  * @param argv Character pointer array pointing to the passed arguments
  * @return 0 When the execution is successful
+ * @return 1 When fatal error stream is reached
  */
 int main(int argc, char **argv) {
-  /// Initialize ROS node
-  ros::init(argc, argv, "talker");
+    int frequency;
+    /// Initialize ROS node
+    ros::init(argc, argv, "talker");
 
-  /// Create an instance of NodeHandle
-  ros::NodeHandle n;
+    /// Check number of arguments to log messages
+    if (argc == 2 || argc == 1) {
+        /// Case when frequency argument is passed
+        if (argc == 2) {
+            frequency = atoi(argv[1]);
+        }
 
-  /// Create a pubisher node to publish 'chatter' topic
-  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
-  ros::ServiceServer server = n.advertiseService("update_string", update);
-
-  ros::Rate loop_rate(10);
-
-  /// Initialize the count
-  int count = 0;
-  while (ros::ok()) {
-    /// Crate string message
-    std_msgs::String msg;
-    std::stringstream ss;
-    ss << msg << count;
-    msg.data = ss.str();
-
-    /// Display the message
-    ROS_INFO("%s", msg.data.c_str());
-
-    /// Publish the message using publisher object
-    chatter_pub.publish(msg);
-
-    /// Command to execute all pending callbacks from all nodes
-    ros::spinOnce();
-    loop_rate.sleep();
-    ++count;
-  }
-  return 0;
+        /// Case when no frequency argument is passed
+        if (argc == 1) {
+            ROS_ERROR_STREAM_ONCE("Arguments missing: 'rate'");
+            ROS_WARN_STREAM("Setting default frequency as 10 Hz");
+            frequency = 10;
+        }
+        /// Case when negative frequency argument is passed
+        if (frequency <= 0) {
+            ROS_FATAL_STREAM("Frequency not valid! Enter positive value: ");
+            ros::shutdown();
+            return 1;
+        } else {
+            /// Create an instance of NodeHandle
+            ros::NodeHandle n;
+            /// Create a pubisher node to publish 'chatter' topic
+            ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+            /// Create a service server node to update the given string
+            ros::ServiceServer server = n.advertiseService("update_string", update);
+            ros::Rate loop_rate(frequency);
+            /// Initialize the count
+            int count = 0;
+            while (ros::ok()) {
+                /// Create debug message to display the frequency value
+                ROS_DEBUG_STREAM("Frequency is: " << frequency);
+                /// Crate string message
+                std_msgs::String msg;
+                std::stringstream ss;
+                ss << text;
+                msg.data = ss.str();
+                /// Display the message
+                ROS_INFO("%s", msg.data.c_str());
+                /// Publish the message using publisher object
+                chatter_pub.publish(msg);
+                /// Command to execute all pending callbacks from all nodes
+                ros::spinOnce();
+                loop_rate.sleep();
+                ++count;
+            }
+            return 0;
+        }
+    }
 }
+
+
+
